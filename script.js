e083adbee43572bc7c2a8e70cd51646c859f88d2
Patrick Fry <patrickfry3@gmail.com>
2026-01-04 01:50:24 -0700

    Big Changes: scraping astronaut, sun, space theme. added more movement and reorganized.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const projectCard = document.querySelectorAll('.project-card');
 
-function notify(){
-    console.log('hello');
-    // card.setAttribute('width', '5px');
-};
 
 projectCard.forEach(card => {
-    card.addEventListener('click', notify);
+    const color = card.querySelector('.project-card-colors');
+
+    card.addEventListener('mouseenter', () => {
+        color.classList.add('spin-fast');
+        card.classList.add('big-card');
+        // card.style.transform = "scale(1.5)";
+
+        color.addEventListener('animationend', () => {
+            color.classList.remove('spin-fast');
+
+            if(card.matches(':hover')){
+                color.classList.add('spin-slow');
+            }
+
+            color.removeEventListener('animationend');
+        });
+    });
+
+    card.addEventListener('click', () => {
+        
+    });
+});
+
+projectCard.forEach(card => {
+    const color = card.querySelector('.project-card-colors');
+    card.addEventListener('mouseleave', () => {
+        color.classList.remove('spin-fast','spin-slow');
+        card.classList.remove('big-card');
+    });
 });
